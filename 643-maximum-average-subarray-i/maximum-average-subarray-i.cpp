518a74337f0b23ab9a3750d89abe6694dbce7a0f
Mritunjay-ranjan <89678656+Mritunjay-ranjan@users.noreply.github.com>
2025-02-16 16:03:19 +0530

    Time: 3 ms (37.38%) | Memory: 113.8 MB (11.88%) - LeetSync

--- 643-maximum-average-subarray-i/maximum-average-subarray-i.cpp
+++ 643-maximum-average-subarray-i/maximum-average-subarray-i.cpp
@@ -1,39 +1,19 @@
-// class Solution {
-// public:
-//     double findMaxAverage(vector<int>& nums, int k) {
-//         double currSum=0;
-//         double maxSum=INT_MIN;
-        
-//         for(int i=0;i<=(nums.size()-k);i++){
-//             currSum=0;
-//             for(int j=i;j<(i+k);j++){
-//                 currSum+=nums[j]; 
-                
-//             }
-//             if(currSum>maxSum){
-//                 maxSum=currSum;
-//             }
-            
-            
-//         }
-        
-//         return maxSum/=k;
-//     }
-// };
 class Solution {
 public:
     double findMaxAverage(vector<int>& nums, int k) {
-        double ans = 0 ; 
-        double sum = 0; 
-        for(int i = 0 ; i< k ; i++){
-            sum += nums[i]; 
+        double max=INT_MIN;
+        double sum=0; 
+        for(int i=0;i<k;i++){
+            sum+=nums[i];
         }
-        ans = sum ;
-        for(int i = k ; i<nums.size() ; i++){
-            sum += nums[i]; 
-            sum -= nums[i-k]; 
-            ans = max(ans , sum );
+        max=sum;
+        for(int i=0;i<nums.size()-k;i++){
+            sum-=nums[i];
+            sum+=nums[i+k];
+            if(sum>max){
+                max=sum;
+            }
         }
-        return ans /k ; 
+        return max/k;
     }
 };
