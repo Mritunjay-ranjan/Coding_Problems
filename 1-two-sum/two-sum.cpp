68af03f6ea113947ff5001aa277d52cf1abbae13
Mritunjay-ranjan <89678656+Mritunjay-ranjan@users.noreply.github.com>
2025-02-20 20:52:53 +0530

    Time: 4 ms (61.09%) | Memory: 16 MB (7.20%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -4,17 +4,13 @@ public:
         unordered_map<int,int>ump;
         vector<int>result;
         for(int i=0;i<nums.size();i++){
+            int complement=target-nums[i];
+            if(ump.count(complement)){
+                result.push_back(ump[complement]);
+                result.push_back(i);
+            }
             ump[nums[i]]=i;
         }
-        for(int i=0;i<nums.size();i++){
-            int complement= target - nums[i];
-            if(ump.count(complement) && ump[complement]!=i){
-                result.push_back(i);
-                result.push_back(ump[complement]);
-                return result;
-            }
-        }
-
         return result;
-        }
-    };
+    }
+};
