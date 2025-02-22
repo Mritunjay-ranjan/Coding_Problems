eeb113d0b4499c985695cde6aaf977ffb9ab2e20
Mritunjay-ranjan <89678656+Mritunjay-ranjan@users.noreply.github.com>
2025-02-22 23:16:08 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (50.60%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.cpp
+++ 54-spiral-matrix/spiral-matrix.cpp
@@ -1,41 +1,37 @@
 class Solution {
 public:
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
-        int m = matrix.size();             // total rows
-        int n = matrix[0].size();          // total columns
-        int offsetRow = 0, offsetCol = 0;    // current top-left offset
-        vector<int> result;
-        
-        while(m > 0 && n > 0) {
-            // 1. Traverse the top row (left to right)
-            for (int j = offsetCol; j < offsetCol + n; j++) {
-                result.push_back(matrix[offsetRow][j]);
+        vector<int>result;
+
+        int left= 0;
+        int right = matrix[0].size();
+        int top= 0;
+        int bottom = matrix.size();
+        while(left <right && top<bottom){
+
+            for(int j=left;j<right;j++){
+                result.push_back(matrix[top][j]);
+            }
+            top++;
+
+            for(int i=top;i<bottom;i++){
+                result.push_back(matrix[i][right-1]);
+            }
+            right--;
+
+            if(!(top<bottom && left<right))
+            break;
+
+            for(int j=right-1;j>=left;j--){
+                result.push_back(matrix[bottom-1][j]);
             }
             
-            // 2. Traverse the right column (top to bottom)
-            for (int i = offsetRow + 1; i < offsetRow + m; i++) {
-                result.push_back(matrix[i][offsetCol + n - 1]);
+            bottom--;
+
+            for(int i=bottom-1;i>=top;i--){
+                result.push_back(matrix[i][left]);
             }
-            
-            // 3. Traverse the bottom row (right to left)
-            if(m > 1) {  // to avoid duplicates if only one row remains
-                for (int j = offsetCol + n - 2; j >= offsetCol; j--) {
-                    result.push_back(matrix[offsetRow + m - 1][j]);
-                }
-            }
-            
-            // 4. Traverse the left column (bottom to top)
-            if(n > 1) {  // to avoid duplicates if only one column remains
-                for (int i = offsetRow + m - 2; i > offsetRow; i--) {
-                    result.push_back(matrix[i][offsetCol]);
-                }
-            }
-            
-            // Move inward to the next layer.
-            offsetRow++;  
-            offsetCol++;
-            m -= 2;       // we’ve processed a top and bottom row
-            n -= 2;       // we’ve processed a left and right column
+            left++;
         }
         return result;
     }
