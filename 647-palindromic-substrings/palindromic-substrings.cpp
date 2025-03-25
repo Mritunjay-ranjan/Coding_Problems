77eab760638ac01b16e77441e49d834a866dee0e
Mritunjay-ranjan <89678656+Mritunjay-ranjan@users.noreply.github.com>
2025-03-26 00:17:53 +0530

    Time: 1396 ms (6.65%) | Memory: 406.6 MB (9.45%) - LeetSync

---
+++ 647-palindromic-substrings/palindromic-substrings.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool checkPalindrome(string s, int i, int j){
+        while(i<=j){
+            if(s[i]!=s[j]){
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+
+    int countSubstrings(string s) {
+        int count=0;
+        for(int i=0;i<s.length();i++){
+            for(int j=i;j<s.length();j++){
+            if(checkPalindrome(s, i, j)){
+                count++;
+            }
+            }
+        }
+        return count;
+    }
+};
