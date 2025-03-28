b6d928080c0ca057cc657219eed5428d55dde32f
Mritunjay-ranjan <89678656+Mritunjay-ranjan@users.noreply.github.com>
2025-03-29 01:17:00 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (74.61%) - LeetSync

--- 807-custom-sort-string/custom-sort-string.cpp
+++ 807-custom-sort-string/custom-sort-string.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
-    // string customSortString(string order, string s) {
-    //     int i=0;
-    //     for(int j=0;j<order.length();j++){
-    //         while(i<s.length() && s.find(order[j],i)!=string::npos){
-    //             swap(s[i],s[s.find(order[j],i)]);    
-    //             i++;
-    //         }
-    //     }
-    //     return s;
-    // }
     string customSortString(string order, string s) {
         int i=0;
         for(int j=0;j<order.length();j++){
-            while(i<s.length() && (s.substr(i,s.length()-1)).find(order[j])!=string::npos){
-                swap(s[i],s[(s.substr(i,s.length()-1)).find(order[j])+i]);    
+            while(i<s.length() && s.find(order[j],i)!=string::npos){
+                swap(s[i],s[s.find(order[j],i)]);    
                 i++;
             }
         }
         return s;
     }
+    // string customSortString(string order, string s) {
+    //     int i=0;
+    //     for(int j=0;j<order.length();j++){
+    //         while(i<s.length() && (s.substr(i,s.length()-1)).find(order[j])!=string::npos){
+    //             swap(s[i],s[(s.substr(i,s.length()-1)).find(order[j])+i]);    
+    //             i++;
+    //         }
+    //     }
+    //     return s;
+    // }
 };
