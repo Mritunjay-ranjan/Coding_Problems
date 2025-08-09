c3d5adeee71329f20f7f8a101cdb8ac553538921
Mritunjay-ranjan <89678656+Mritunjay-ranjan@users.noreply.github.com>
2025-08-09 15:44:47 +0530

    Time: 4 ms (11.35%) | Memory: 41.9 MB (47.50%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,15 +1,13 @@
 class Solution {
     public boolean isValid(String s) {
-        HashMap<Character, Character> mappedBrackets = new HashMap<>();
-        mappedBrackets.put(')', '(');
-        mappedBrackets.put('}', '{');
-        mappedBrackets.put(']', '[');
-
+        HashMap<Character, Character> bracketMap = new HashMap<>();
+        bracketMap.put('}','{');
+        bracketMap.put(')','(');
+        bracketMap.put(']','[');
         Stack<Character> stack = new Stack<>();
-
         for(int i=0;i<s.length();i++){
             char c = s.charAt(i);
-            if(mappedBrackets.containsValue(c)){
+            if(bracketMap.containsValue(c)){
                 stack.push(c);
             }
             else{
@@ -17,8 +15,7 @@ class Solution {
                     return false;
                 }
                 char top = stack.pop();
-                
-                if(top!=mappedBrackets.get(c)){
+                if(top!=bracketMap.get(c)){
                     return false;
                 }
             }
