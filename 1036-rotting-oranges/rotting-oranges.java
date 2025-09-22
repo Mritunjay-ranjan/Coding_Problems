a696b6d9ecfcc5536d17c765220732c3b7daa3f8
Mritunjay-ranjan <89678656+Mritunjay-ranjan@users.noreply.github.com>
2025-09-22 06:48:36 +0530

    Time: 1 ms (100.00%) | Memory: 42.4 MB (75.83%) - LeetSync

---
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -0,0 +1,43 @@
+class Solution {
+    public int orangesRotting(int[][] grid) {
+        if(grid==null || grid.length==0) return -1;
+        int m = grid.length;
+        int n = grid[0].length;
+        int freshCount=0;
+        Queue<int[]> rottenQueue = new LinkedList<>();
+
+        //count fresh oranges and enqueue rotten oranges
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j]==1){
+                    freshCount++;
+                }
+                else if(grid[i][j]==2){
+                    rottenQueue.offer(new int[]{i,j});
+                }
+            }
+        }
+        if(freshCount==0) return 0; //No fresh oranges
+        int minutes = 0;
+        int[][] directions = {{1,0},{0,1},{-1,0},{0,-1}};
+
+        //BFS
+        while(!rottenQueue.isEmpty()){
+            int size = rottenQueue.size();
+            for(int i=0;i<size;i++){
+                int[] rotten = rottenQueue.poll();
+                for(int[] dir: directions){
+                    int x= rotten[0]+dir[0];
+                    int y= rotten[1]+dir[1];
+                    if(x>=0 && x<m && y>=0 && y<n && grid[x][y]==1){
+                        grid[x][y]=2; //Turn fresh orange into rotten
+                        freshCount--;
+                        rottenQueue.offer(new int[]{x,y});
+                    }
+                }
+            }
+            minutes++; //increment timer after each level of BFS
+        }
+        return freshCount==0? minutes-1: -1;
+    }
+}
